cae4453ff949340857eb093678063e6919737699
tejasprakash <tejas.prakash@celestialsys.com>
2024-08-20 22:51:55 +0530

    new add

--- EKS/main.tf
+++ EKS/main.tf
@@ -54,16 +54,10 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-   depends_on = [
-    module.eks
-  ]
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id 
-  depends_on = [
-    module.eks
-  ]
+  name = module.eks.cluster_name 
 }
 
