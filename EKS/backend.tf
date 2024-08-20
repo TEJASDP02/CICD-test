9f029b4a340dcaf7ccc56522b435c682d8f57b6c
tejasprakash <tejas.prakash@celestialsys.com>
2024-08-20 19:58:21 +0530

    added 2

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "cicd-terraform-eks-poc"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "cicd-terraform-eks-poc"
     key    = "jenkins/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
